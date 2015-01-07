b03e59b3edece835ea45d0cd1cd195bffb62dc78
simonorlovsky <simon.orlovsky@yahoo.com>
2015-01-06 20:30:20 -0600

    Made datastructures review

---
+++ datastructures/dictionary.py
@@ -0,0 +1,10 @@
+#Simon Orlovsky
+#Winter 2015
+#python dictionary practice
+
+
+def main():
+	tel = {'jack': 4098, 'sape': 4139}
+	print tel['jack']
+
+main()
--- ps01.py
+++ ps01.py
@@ -3,8 +3,29 @@
 YES = [0,1,1,2,2,3,3,9,9]
 EW = [0,1,1,1,4,4,4,5,5,5,8]
 
-def presort(yes, ew):
-	return YESArray, EWArray
+def pre(yes, ew):
+	YESready = []
+	EWready = []
+	for i in range(max(yes)):
+		YESready.append(0)
+	for i in range(max(ew)):
+		EWready.append(0)
+
+	for i in range(len(YES)):
+		YESready[YES[i]-1] = YESready[YES[i]-1] + 1
+
+	for i in range(len(EW)):
+		EWready[EW[i]-1] = EWready[EW[i]-1] + 1
+
+
+	return YESready, EWready
+
+def post(YESready, EWready):
+	return TRUE
 
 def main():
-	print "Hello world"
+	YESready, EWready = pre(YES, EW)
+	print YESready
+	print EWready
+
+main()
